ba8e05da97b805e4f7b3b42d07017efeb47684d2
rihamissa123 <riham.issa123@gmail.com>
2023-07-06 17:39:24 +0400

    Creating Students data in a table

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-function GetAllStudents(){
 
-fetch("https://test20230706-1x06.onrender.com/getAllStudents")
-    .then(response=>response.json())
-    .then(json=>{
-        let studentsul=document.getElementById("students");
-        json.forEach(s=>{
-            let studentsli=document.createElement("li");
-            studentsli.innerHTML=s.name;
-            studentsul.appendChild(studentsli);})
-    })
-}
+window.addEventListener('DOMContentLoaded', (event) => {
+    fetch('http://localhost:3030/getAllStudents')
+      .then(response => response.json())
+      .then(data => {
+        const table = document.getElementById('studentTable');
+  
+        data.forEach(student => {
+          const row = document.createElement('tr');
+          const nameCell = document.createElement('td');
+          const idCell = document.createElement('td');
+  
+          nameCell.textContent = student.name;
+          idCell.textContent = student.id;
+  
+          row.appendChild(nameCell);
+          row.appendChild(idCell);
+          table.appendChild(row);
+        });
+      })
+      .catch(error => console.error('Error:', error));
+  });
+  
