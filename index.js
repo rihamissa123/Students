80a505fb130f86189427f60b88fb6e09610baa89
rihamissa123 <riham.issa123@gmail.com>
2023-07-06 18:32:27 +0400

    Final Modifications

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    fetch('https://test20230706-1x06.onrender.com')
+    fetch('https://students-klmu.onrender.com/getAllStudents')
       .then(response => response.json())
       .then(data => {
         const table = document.getElementById('studentTable');
